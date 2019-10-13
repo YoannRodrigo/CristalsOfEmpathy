8ceef6b6541922f6f2d88ad9f6bc484b85e3431f
Yoann <yomaisonneuve@gmail.com>
2019-10-13 17:49:14 +0200

    #119 : Add UI Player choice in Main Menu

---
+++ CristalsOfEmpathy/Assets/Scripts/Managers/GeneralGameManager.cs
@@ -0,0 +1,23 @@
+﻿#region Using Directives
+
+using UnityEngine;
+
+#endregion
+
+public class GeneralGameManager : MonoBehaviour
+{
+    #region Member Variables
+
+    private static GameObject _playerPrefab;
+
+    #endregion
+
+    #region Methods
+
+    public void SetPlayerPrefab(GameObject playerPrefab)
+    {
+        _playerPrefab = playerPrefab;
+    }
+
+    #endregion
+}
