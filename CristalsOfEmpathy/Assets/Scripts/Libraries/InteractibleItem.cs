7a84dea41d9628a3318f5a3557f4203f559743a8
Yoann <yomaisonneuve@gmail.com>
2019-10-14 15:20:45 +0200

    #130 : Add interactible item (parent)
    Item send debug log on touch

---
+++ CristalsOfEmpathy/Assets/Scripts/Libraries/InteractibleItem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class InteractibleItem : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            if (Camera.main != null)
+            {
+                Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                if (Physics.Raycast(raycast, out RaycastHit raycastHit))
+                {
+                    if (raycastHit.collider.gameObject == gameObject)
+                    {
+                        Debug.Log("Touch");
+                    }
+                }
+            }
+        }
+    }
+}
