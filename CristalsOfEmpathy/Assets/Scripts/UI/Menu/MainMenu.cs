10abe4be6f0d0d92d4eb07d79c4f4fc233d9bda3
yanicetrech <yanice.trech@hotmail.com>
2019-10-14 13:45:51 +0200

    adding features #121

--- CristalsOfEmpathy/Assets/Scripts/UI/Menu/MainMenu.cs
+++ CristalsOfEmpathy/Assets/Scripts/UI/Menu/MainMenu.cs
@@ -1,6 +1,9 @@
 ﻿#region Using Directives
 
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 #endregion
 
@@ -8,23 +11,49 @@ public class MainMenu : MonoBehaviour
 {
     #region Member Variables
 
+    public Button playButton;
+    public Button optionsButton;
+    public Button backButton;
+
+    public GameObject playerSubMenu;
+    public GameObject optionsSubMenu;
+    public GameObject mainMenu;
+
     #endregion
 
     #region Methods
 
     public void PlayGameOnClick()
     {
-        
-    }
-
-    public void QuitGameOnClick()
-    {
-        Debug.Log("Quit");
-        Application.Quit();
+        if (playButton)
+        {
+            mainMenu.gameObject.SetActive(false);
+            playerSubMenu.gameObject.SetActive(true);
+        }
     }
 
     public void OptionsGameOnClick()
     {
+        if (optionsButton)
+        {
+            mainMenu.gameObject.SetActive(false);
+            optionsSubMenu.gameObject.SetActive(true);
+        }
+        
+    }
+
+    public void BackOptionsOnClick()
+    {
+        if (backButton)
+        {
+            optionsSubMenu.SetActive(false);
+            mainMenu.gameObject.SetActive(true);
+        }
+    }
+    public void QuitGameOnClick()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
     }
 
     #endregion
