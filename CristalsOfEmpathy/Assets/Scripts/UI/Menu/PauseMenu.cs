70eb0cb95f0d1fb479d4c855e8bc64a822068c33
yanicetrech <yanice.trech@hotmail.com>
2019-10-14 16:03:18 +0200

    feature #132 - Adding PAUSE menu in game (may required a fix for tablet/phone)

---
+++ CristalsOfEmpathy/Assets/Scripts/UI/Menu/PauseMenu.cs
@@ -0,0 +1,62 @@
+﻿#region Using Directives
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+#endregion
+
+public class PauseMenu : MonoBehaviour
+{
+    #region Member Variables
+
+    public GameObject pauseMenu;
+    public GameObject optionsSubMenu;
+    public GameObject joystickButton;
+
+    public Button pauseButton;
+    public Button backButton;
+    public Button optionsButton;
+    public Button menuButton;
+    public Button backOptionsSubMenuOptions;
+
+    #endregion
+
+    #region Methods
+    public void PauseButtonOnClick()
+    {
+        pauseMenu.gameObject.SetActive(true);
+        joystickButton.gameObject.SetActive(false);
+        pauseButton.gameObject.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    public void BackToGameOnClick()
+    {
+        pauseMenu.gameObject.SetActive(false);
+        joystickButton.gameObject.SetActive(true);
+        pauseButton.gameObject.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    public void OptionsGameOnClick()
+    {
+        pauseMenu.gameObject.SetActive(false);
+        optionsSubMenu.gameObject.SetActive(true);
+    }
+
+    public void BackToMenuOnClick()
+    {
+        pauseMenu.gameObject.SetActive(true);
+        optionsSubMenu.gameObject.SetActive(false);
+    }
+
+    public void MenuGameOnClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + -2);
+    }
+
+    #endregion
+}
