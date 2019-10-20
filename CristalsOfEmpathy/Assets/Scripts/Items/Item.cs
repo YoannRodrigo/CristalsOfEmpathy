f6d98a06cac49b78ab888cd530545e41d12dff78
yanicetrech <yanice.trech@hotmail.com>
2019-10-20 20:41:09 +0200

    FIXING Pushing issues of the feature#133

---
+++ CristalsOfEmpathy/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,27 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Item
+//{
+//    public int id;
+//    public string title;
+//    public string description;
+//    public Sprite itemIcon;
+
+//    public Item(int id, string title, string description)
+//    {
+//        this.id = id;
+//        this.title = title;
+//        this.description = description;
+//        this.itemIcon = Resources.Load<Sprite>("Sprites/Itemp/" + title);
+//    }
+
+//    public Item(Item item)
+//    {
+//        this.id = item.id;
+//        this.title = item.title;
+//        this.description = item.description;
+//        this.itemIcon = Resources.Load<Sprite>("Sprites/Itemp/" + item.title);
+//    }
+//}
---
+++ CristalsOfEmpathy/Assets/Scripts/UI/Menu/InventoryMenu.cs
@@ -0,0 +1,42 @@
+﻿#region Using Directives
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#endregion
+
+
+public class InventoryMenu : MonoBehaviour
+{
+    #region Member Variables
+
+    public GameObject inventoryMenu;
+
+    public Button inventoryButton;
+
+    private bool isOpen = false;
+
+    #endregion
+
+    #region Methods
+
+    public void InventoryButtonOnClick()
+    {
+        if (!isOpen)
+        {
+            inventoryMenu.SetActive(true);
+            isOpen = true;
+        }
+        else
+        {
+            inventoryMenu.SetActive(false);
+            isOpen = false;
+        }
+
+    }
+
+    #endregion
+
+}
