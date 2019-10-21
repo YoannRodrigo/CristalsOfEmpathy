099c290d1dac6b404a5a48769ee6726af870b727
Yoann <yomaisonneuve@gmail.com>
2019-10-21 17:58:04 +0200

    #156 : Add screen between main menu and game scene

---
+++ CristalsOfEmpathy/Assets/Scripts/Managers/SplashScreenToGame.cs
@@ -0,0 +1,28 @@
+﻿#region Using Directives
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#endregion
+public class SplashScreenToGame : MonoBehaviour
+{
+    #region Member Variables
+
+    private const float TIME_MAX_BEFORE_NEXT_SCENE = 2f;
+    private float timeSinceSceneStarted;
+    
+    #endregion
+    
+    #region Methods
+    
+    private void Update()
+    {
+        timeSinceSceneStarted += Time.deltaTime;
+        if (timeSinceSceneStarted > TIME_MAX_BEFORE_NEXT_SCENE)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+    
+    #endregion
+}
