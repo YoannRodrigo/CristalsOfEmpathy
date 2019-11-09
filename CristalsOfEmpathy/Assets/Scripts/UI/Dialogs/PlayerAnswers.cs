8e69c8c40ccbb68d97ad9fc8a694102e2415d7a3
Yoann <yomaisonneuve@gmail.com>
2019-11-09 15:53:13 +0100

    #154 : Temp dialogue UI + Multiple answer

--- CristalsOfEmpathy/Assets/Scripts/UI/Dialogs/PlayerAnswers.cs
+++ CristalsOfEmpathy/Assets/Scripts/UI/Dialogs/PlayerAnswers.cs
@@ -3,14 +3,12 @@ using UnityEngine;
 #endregion
 
 [System.Serializable]
-public class Dialogue
+public class PlayerAnswers
 {
     #region Member Variables
 
-    public string pnjName;
-    public bool isAnswerNeeded;
-    public int nextTextId;
+    public int nextTextId1, nextTextId2, nextTextId3, nextTextId4;
     [TextArea(3, 10)] 
-    public string sentence;
+    public string text1, text2, text3, text4;
     #endregion
 }
