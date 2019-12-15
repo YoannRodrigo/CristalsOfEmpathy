86fd03375ad4b0b4d746d93e17c170a858efd574
Yoann <yomaisonneuve@gmail.com>
2019-12-15 23:17:03 +0100

    #222 : Add light fade out

--- CristalsOfEmpathy/Assets/Scripts/Enigma/Fear/SpawnManager.cs
+++ CristalsOfEmpathy/Assets/Scripts/Enigma/Fear/SpawnManager.cs
@@ -11,93 +11,112 @@ public class SpawnManager : MonoBehaviour
     private float timeSinceBegin;
     private readonly List<float> timeCode = new List<float>(20){6,16,20,22,24,28,37,41,44,48,50,52,60,66,72,78,84,90,96,102};
     private int ghostId;
+    private bool isMusicPlaying;
+    public Light ambiantLight;
 
-    private void Start()
+    private void StartMusic()
     {
-        AkSoundEngine.PostEvent("StartFearMusic", gameObject);
+        if(!isMusicPlaying)
+        {
+            isMusicPlaying = true;
+            AkSoundEngine.PostEvent("StartFearMusic", gameObject);
+        }
     }
     private void Update()
     {
-        timeSinceBegin += Time.deltaTime;
-        if (ghostId < timeCode.Count && timeSinceBegin >= timeCode[ghostId])
+        if (ambiantLight.intensity > 0)
         {
-            switch (ghostId)
+            ambiantLight.intensity -= Time.deltaTime;
+        }
+        else
+        {
+            StartMusic();
+            timeSinceBegin += Time.deltaTime;
+            if (ghostId < timeCode.Count && timeSinceBegin >= timeCode[ghostId])
             {
-                case 0 :
-                    SpawnGhost(1, 7, 6);
-                    break;
-                case 1 : 
-                case 8 :
-                    SpawnGhost(1, 7, 0);
-                    break;
-                case 2 :
-                case 3 :
-                case 7 :
-                    SpawnGhost(1, 7, 2);
-                    break;
-                case 4 :
-                case 10 :
-                    SpawnGhost(1, 7, 4);
-                    break;
-                case 5 :
-                case 9 :
-                    SpawnGhost(1, 7, 3);
-                    break;
-                case 6 :
-                    SpawnGhost(1, 7, 7);
-                    break;
-                case 11 :
-                    SpawnGhost(1, 7, 5);
-                    break;
-                case 12 :
-                    SpawnGhost(1, 5, 1);
-                    SpawnGhost(1, 5, 2);
-                    SpawnGhost(1, 5, 3);
-                    break;
-                case 13 :
-                    SpawnGhost(1, 5, 5);
-                    SpawnGhost(1, 5, 6);
-                    SpawnGhost(1, 5, 7);
-                    break;
-                case 14 :
-                    SpawnGhost(1, 5, 0);
-                    SpawnGhost(1, 5, 1);
-                    SpawnGhost(1, 5, 7);
-                    break;
-                case 15 :
-                    SpawnGhost(1, 5, 3);
-                    SpawnGhost(1, 5, 4);
-                    SpawnGhost(1, 5, 5);
-                    break;
-                case 16 :
-                    SpawnGhost(1, 5, 1);
-                    SpawnGhost(1, 5, 3);
-                    SpawnGhost(1, 5, 5);
-                    SpawnGhost(1, 5, 7);
-                    break;
-                case 17 :
-                    SpawnGhost(1, 5, 0);
-                    SpawnGhost(1, 5, 2);
-                    SpawnGhost(1, 5, 4);
-                    SpawnGhost(1, 5, 6);
-                    break;
-                case 18 :
-                    SpawnGhost(3, 5, 2);
-                    break;
-                case 19 :
-                    SpawnGhost(4, 5, 6);
-                    break;
-                
+                SpawnGhost();
             }
         }
     }
 
-    private void SpawnGhost(float timeBeforeDeath, float timeToReachPlayer, int spawnIndex)
+    private void SpawnAGhost(float timeBeforeDeath, float timeToReachPlayer, int spawnIndex)
     {
         GameObject ghost = Instantiate(ghostPrefab, spawnersTransform[spawnIndex].position, spawnersTransform[spawnIndex].rotation);
         ghost.GetComponent<GhostController>().SetPlayerTransform(playerTransform);
         ghost.GetComponent<GhostController>().SetDifficulty(timeBeforeDeath,timeToReachPlayer);
         ghost.SetActive(true);
         ghostId++;
+    }
+
+    private void SpawnGhost()
+    {
+        switch (ghostId)
+                {
+                    case 0:
+                        SpawnAGhost(1, 7, 6);
+                        break;
+                    case 1:
+                    case 8:
+                        SpawnAGhost(1, 7, 0);
+                        break;
+                    case 2:
+                    case 3:
+                    case 7:
+                        SpawnAGhost(1, 7, 2);
+                        break;
+                    case 4:
+                    case 10:
+                        SpawnAGhost(1, 7, 4);
+                        break;
+                    case 5:
+                    case 9:
+                        SpawnAGhost(1, 7, 3);
+                        break;
+                    case 6:
+                        SpawnAGhost(1, 7, 7);
+                        break;
+                    case 11:
+                        SpawnAGhost(1, 7, 5);
+                        break;
+                    case 12:
+                        SpawnAGhost(1, 5, 1);
+                        SpawnAGhost(1, 5, 2);
+                        SpawnAGhost(1, 5, 3);
+                        break;
+                    case 13:
+                        SpawnAGhost(1, 5, 5);
+                        SpawnAGhost(1, 5, 6);
+                        SpawnAGhost(1, 5, 7);
+                        break;
+                    case 14:
+                        SpawnAGhost(1, 5, 0);
+                        SpawnAGhost(1, 5, 1);
+                        SpawnAGhost(1, 5, 7);
+                        break;
+                    case 15:
+                        SpawnAGhost(1, 5, 3);
+                        SpawnAGhost(1, 5, 4);
+                        SpawnAGhost(1, 5, 5);
+                        break;
+                    case 16:
+                        SpawnAGhost(1, 5, 1);
+                        SpawnAGhost(1, 5, 3);
+                        SpawnAGhost(1, 5, 5);
+                        SpawnAGhost(1, 5, 7);
+                        break;
+                    case 17:
+                        SpawnAGhost(1, 5, 0);
+                        SpawnAGhost(1, 5, 2);
+                        SpawnAGhost(1, 5, 4);
+                        SpawnAGhost(1, 5, 6);
+                        break;
+                    case 18:
+                        SpawnAGhost(3, 5, 2);
+                        break;
+                    case 19:
+                        SpawnAGhost(4, 5, 6);
+                        break;
+
+                }
     }
 }
