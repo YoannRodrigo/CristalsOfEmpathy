5aa1eb0dec840c00ee24d41e65fb1365c187f511
yanicetrech <yanice.trech@hotmail.com>
2020-01-31 20:44:40 +0100

    FEATURE/#234 - Adding UI RUNES/VICTORY SCREEN / Changing the UI while touching a rune / runes counting detection for UI purpose

--- CristalsOfEmpathy/Assets/Scripts/Enigma/Curiosity/InteractiblePotion.cs
+++ CristalsOfEmpathy/Assets/Scripts/Enigma/Curiosity/InteractiblePotion.cs
@@ -1,13 +1,35 @@
 ﻿#region Using Directives
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 #endregion
 
 public class InteractiblePotion : MonoBehaviour
 {
     #region Member Variables
+    public RunesCounter runesRef;
+
     public GameObject nextRune;
     public GameObject victoryScreen;
     public GameObject nearParticlesSystem;
+
+    public Image rune1;
+    public Image rune1Filled;
+
+    public Image rune2;
+    public Image rune2Filled;
+
+    public Image rune3;
+    public Image rune3Filled;
+
+    public Image rune4;
+    public Image rune4Filled;
+
+    public Image rune5;
+    public Image rune5Filled;
+
+    public Image rune6;
+    public Image rune6Filled;
 
     public LevelChanger levelChanger;
     #endregion
@@ -32,28 +54,57 @@ public class InteractiblePotion : MonoBehaviour
                 if (raycastHit.collider.gameObject == gameObject && !PauseMenu.IsOnPause())
                 {
                     CuriosityEnigmaProgression();
+                    RunesProgression();
                 }
             }
         }
     }
 
-    private void CuriosityEnigmaProgression()
+    public void CuriosityEnigmaProgression()
     {
         if (nextRune != null)
         {
-            Debug.Log("Object Touched");
             Destroy(this.gameObject);
             nextRune.SetActive(true);
-        }
-
-        else
-        {
-            Debug.Log("Finished");
-            Destroy(this.gameObject);
-            victoryScreen.SetActive(true);
-            levelChanger.ChangeToLevelWithFade(0);
+            runesRef.runesCount++;
+            Debug.Log("Object Touched");
+            Debug.Log(runesRef.runesCount);
         }
     }
 
+    public void RunesProgression()
+    {
+        switch (runesRef.runesCount)
+        {
+            case 1:
+                rune1Filled.gameObject.SetActive(true);
+                break;
+            case 2:
+                rune2Filled.gameObject.SetActive(true);
+                break;
+            case 3:
+                rune3Filled.gameObject.SetActive(true);
+                break;
+            case 4:
+                rune4Filled.gameObject.SetActive(true);
+                break;
+            case 5:
+                rune5Filled.gameObject.SetActive(true);
+                break;
+            case 6:
+                rune6Filled.gameObject.SetActive(true);
+                victoryScreen.SetActive(true);
+                StartCoroutine(WaitToReturn());
+                break;
+            default:
+                break;
+        }
+    }
+
+    public IEnumerator WaitToReturn()
+    {
+        yield return new WaitForSeconds(2);
+        levelChanger.ChangeToLevelWithFade(0);
+    }
     #endregion
 }
