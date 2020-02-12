454a25a7f8d76272dd5a41cf318b3ddc7bc3f276
Barkintosh <l.perdu@rubika-edu.com>
2020-02-13 00:22:42 +0100

    Deleting unused and confusing variable

--- CristalsOfEmpathy/Assets/Scripts/Managers/GeneralGameManager.cs
+++ CristalsOfEmpathy/Assets/Scripts/Managers/GeneralGameManager.cs
@@ -13,7 +13,6 @@ public class GeneralGameManager : MonoBehaviour
     public GameObject cameraPrefab;
     public GameObject playerPrefab;
 
-    public Transform playerSpawnerTransform;
     private static int _playerPrefabChoice = 0;
     private int nextPortalIndex = 0;
     #endregion
