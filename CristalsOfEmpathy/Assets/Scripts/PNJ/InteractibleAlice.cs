2e76d7a22b60b88c70018e7329f76268fb4c6157
Yoann <yomaisonneuve@gmail.com>
2020-02-17 15:59:17 +0100

    Add Scripts for important met PNJ

---
+++ CristalsOfEmpathy/Assets/Scripts/PNJ/InteractibleAlice.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractibleAlice : InteractiblePnj
+{
+    protected override void OnTouch()
+    {
+        base.OnTouch();
+        GeneralGameManager.instance.hasPlayerMetAlice = true;
+    }
+}
