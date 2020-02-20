a9a02eacf33443e844998268beb428e67ffc5960
Yoann <yomaisonneuve@gmail.com>
2020-02-20 13:31:24 +0100

    Add Forains Quest

--- CristalsOfEmpathy/Assets/Scripts/PNJ/InteractibleForains.cs
+++ CristalsOfEmpathy/Assets/Scripts/PNJ/InteractibleForains.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class InteractibleForains : InteractiblePnj
 {
+    public ScriptablePNJ dialogueQuest;
     public InteractibleForains otherForain;
 
     public override void OnDialogEnded()
     {
         base.OnDialogEnded();
         otherForain.dialogue = otherForain.dialogueIdle;
+        if (GeneralGameManager.instance.hasPlayerAcceptedBartenderQuest)
+        {
+            GeneralGameManager.instance.isBartenderQuestFinished = true;
+        }
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        if (GeneralGameManager.instance.hasPlayerAcceptedBartenderQuest &&
+            !GeneralGameManager.instance.isBartenderQuestFinished)
+        {
+            dialogue = dialogueQuest;
+            otherForain.dialogue = otherForain.dialogueQuest;
+        }
     }
 }
