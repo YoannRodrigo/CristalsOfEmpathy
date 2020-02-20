b65992614ae5294c0771463462d7bad947bf6a60
Yoann <yomaisonneuve@gmail.com>
2020-02-20 02:06:26 +0100

    Update Mages zone and Build Settings

---
+++ CristalsOfEmpathy/Assets/Scripts/PNJ/InteractibleMages.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractibleMages : InteractiblePnj
+{
+    public List<InteractibleMages> otherMages = new List<InteractibleMages>();
+
+    public override void OnDialogEnded()
+    {
+        base.OnDialogEnded();
+        foreach (InteractibleMages otherMage in otherMages)
+        {
+            otherMage.dialogue = otherMage.dialogueIdle;
+        }
+    }
+}
