75b3b22ead00d77355d65188402bdd3d88e5301b
Yoann <yomaisonneuve@gmail.com>
2020-02-20 03:10:19 +0100

    Add faces and Nuien/Muien in Florist scene

---
+++ CristalsOfEmpathy/Assets/Scripts/PNJ/InteractiblePolice.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiblePolice : InteractiblePnj
+{
+    public InteractiblePolice otherPolice;
+
+    public override void OnDialogEnded()
+    {
+        base.OnDialogEnded();
+        otherPolice.dialogue = otherPolice.dialogueIdle;
+    }
+}
